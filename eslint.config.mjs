8f5e89641abc4a603188c77b19ceed8bd5abb0d3
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-09-22 10:32:30 +0500

    fix(build)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,9 @@
 // @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
 
-import pluginVue from 'eslint-plugin-vue'
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
-let withNuxt = []
-
-try {
-  // Nuxt generates this after `nuxi prepare` or `nuxi dev`
-  withNuxt = (await import('./.nuxt/eslint.config.mjs')).default
-} catch {
-  console.warn('⚠️  Skipping Nuxt ESLint config because `.nuxt/eslint.config.mjs` was not found')
-}
-
-export default [
-  ...withNuxt,
-  {
-    files: ['**/*.vue', '**/*.js', '**/*.ts'],
-    plugins: { vue: pluginVue },
-    rules: {
-      'vue/no-multiple-template-root': 'off',
-      'vue/max-attributes-per-line': ['error', { singleline: 3 }]
-    }
+export default withNuxt({
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    'vue/max-attributes-per-line': ['error', { singleline: 3 }]
   }
-]
+})
