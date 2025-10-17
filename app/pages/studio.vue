d1b0433a8647fe565dba63baca679f56e3c0b100
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-10-17 14:52:07 +0500

    feat(register and add models)

--- app/pages/studio.vue
+++ app/pages/studio.vue
@@ -108,7 +108,7 @@
             @reload="() => { }" />
 
           <template #footer>
-            <ModelSelect v-model="model" />
+            <ModelSelect v-model="model"  :models="models"/>
           </template>
         </UChatPrompt>
       </UContainer>
@@ -132,7 +132,7 @@ const currentChatId = ref<string | null>(null)
 const input = ref('')
 
 // composable
-const { chats, messages, isLoading, sendMessage, clearMessages, getHistory, loadChat, getModels, createConversation } = useAI()
+const { chats, messages, models, isLoading, sendMessage, clearMessages, getHistory, loadChat, getModels, createConversation } = useAI()
 const error = computed(() => {
   const errorMessage = useAI().error?.value;
   return errorMessage ? new Error(errorMessage) : undefined;
